a104346bfad74719f08e89f5283b498100f3a492
Saumya Sadh <ssadh@ucsd.edu>
2025-04-29 23:05:19 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,13 +7,10 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
-
+        let result = 0;
         result = num1 + num2;
-
         return result;
-    }
-    else {
+    } else {
         return !add;
     }
 }
@@ -25,15 +22,15 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
-    const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+    if (prices.length === 0) return false;
+
+    const discounted = [];
+    for (let i = 0; i < prices.length; i++) {
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
     return discounted;
 }
 
-module.exports = {sumValues, discountPrices};
+module.exports = { sumValues, discountPrices };
